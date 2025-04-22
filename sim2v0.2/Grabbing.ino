fc6fc5a42471349367f3be5d179f02877fddd6d6
Martin-fonty <564266@student.fontys.nl>
2025-04-22 10:03:37 +0200

    added fail safe for grabber

--- sim2v0.2/Grabbing.ino
+++ sim2v0.2/Grabbing.ino
@@ -3,10 +3,14 @@ void grab()
   int gripAngle = 90;
   int i = 0;
   do {
-    servoGrip.write(gripAngle - i); //close gripper slightly
-    i++;
+    servoGrip.write(gripAngle--); //close gripper slightly
+    
     pressureValue = analogRead(FSR_PIN); //read pressure sensor
     Serial.print("Pressure Value: ");
     Serial.println(pressureValue);
+
+    if(gripAngle <= 0){
+      break;
+    }
   }
   while(pressureValue == thresholdPress); //check if gripper is gripping
